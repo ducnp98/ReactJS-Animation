02f3828d4a0d1d9305184182bb69d900a4a6e030
ducnp98 <duc.nguyenphuoc160198@gmail.com>
2024-05-16 14:39:40 +0700

    16/5/2024: Import tailwind and material tailwind, import font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Avatar } from "@material-tailwind/react";
 
 const App = () => {
   return (
     <div className="App">
-      <p>Hello xin chao</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Avatar src="https://docs.material-tailwind.com/img/face-2.jpg" alt="avatar" />
     </div>
   );
 };
